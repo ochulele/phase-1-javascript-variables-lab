325b16dab278d1fbcb6993d0be5b5563bdfb2043
ochuk <ochuciano@gmail.com>
2023-03-30 13:18:30 -0400

    javascript variables

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+//companyName = 'Scuber';
+
+const companyName = 'Scuber';
+
+mostProfitableNeighborhood = 'Chelsea';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+companyCeo = 'Susan Smith';
+let companyCeo = 'Susan Smith';
